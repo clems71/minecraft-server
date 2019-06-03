f45f14f0c24337c5e1a436bbbb9fd6e0d8712a8b
Clement JACOB <clems71@gmail.com>
2019-06-03 22:29:33 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,19 +2,19 @@ echo "eula=true" > eula.txt
 
 if [ -n "$MINECRAFT_OPS_JSON" ]
 then
-    echo $MINECRAFT_OPS_JSON > ./ops.json
+    echo "$MINECRAFT_OPS_JSON" > ./ops.json
 fi
 
 if [ -n "$MINECRAFT_WHITELIST_JSON" ]
 then
-    echo $MINECRAFT_WHITELIST_JSON > ./whitelist.json
+    echo "$MINECRAFT_WHITELIST_JSON" > ./whitelist.json
 fi
 
 if [ -z "$MINECRAFT_SERVER_PROPS" ]
 then
     cp /srv/defaults/server.properties .
 else
-    echo $MINECRAFT_SERVER_PROPS > ./server.properties
+    echo "$MINECRAFT_SERVER_PROPS" > ./server.properties
 fi
 
 java -Xmx1024M -Xms1024M -jar /srv/minecraft_server.1.14.2.jar nogui
